8b0e91c87a8a41957e673cbbaf447099935a9785
apisitbase <41176856+apisitbase@users.noreply.github.com>
2018-08-01 11:57:38 +0700

    Update ESP_netpie_linebot.ino

--- ESP_netpie_linebot.ino
+++ ESP_netpie_linebot.ino
@@ -1,105 +1,135 @@
 #include <ESP8266WiFi.h>
 #include <MicroGear.h>
-#include <ESP8266HTTPClient.h>
-#include <ArduinoJson.h>
-const char* ssid     = "TOPIC_DEV"; //change this to your SSID
-const char* password = "1212312121"; //change this to your PASSWORD
+#include "DHT.h"              // library สำหรับอ่านค่า DHT Sensor
 
-const char* host = "https://botstree.herokuapp.com/bot.php";//change this to your linebot server ex.http://numpapick-linebot.herokuapp.com/bot.php
-#define APPID   "Basebot"     //change this to your APPID
-#define KEY     "VL3Ntfnptg9LOOh"     //change this to your KEY
-#define SECRET  "Q7vloIhwHWBeCYOJkb5YT3qGM"     //change this to your SECRET
+//const char* ssid     = "AndroidAP";                  // ชื่อ ssid
+//const char* password = "base1234";              // รหัสผ่าน wifi
+const char* ssid     = "HUAWEI Mate 10 Pro";                  // ชื่อ ssid
+const char* password = "75117411";   
+#define APPID   "wait"                              // ให้แทนที่ด้วย AppID
+#define KEY     "TQcw7EnGTmKr9sX"                                // ให้แทนที่ด้วย Key
+#define SECRET  "Uw4TjSkL7gzYKCPyVyMYgnJC9"                             // ให้แทนที่ด้วย Secret
+#define ALIAS   "esp8266"                              // ชื่ออุปกรณ์
+//#define ALIAS  "FBwait"
+//#def ine neighbor "neighbor"                             // ชื่ออุปกรณ์ที่ต้องการส่งข้อความไปให้
+#define topicPublish "/dht/" ALIAS                        // topic ที่ต้องการ publish ส่งข้อความ
 
-#define ALIAS   "Botstree" //set name of drvice
-#define TargetWeb "switch" //set target name of web
+#define SWITCHPIN 4      // Switch pin
+#define LEDPIN 5         // LED pin
+#define DHTPIN 12         // GPIO2 ขาที่ต่อเข้ากับขา DATA(OUT) ของ DHT
+#define DHTTYPE DHT22     // e.g. DHT11, DHT21, DHT22
+DHT dht(DHTPIN, DHTTYPE);
+
+unsigned long lastTimeDHT = 0;
+float humid = 0;
+float temp = 0;
+int stateLED = 0;
+bool stateChange = false;
 
 WiFiClient client;
-String uid = "";
+
 int timer = 0;
 MicroGear microgear(client);
 
 void onMsghandler(char *topic, uint8_t* msg, unsigned int msglen) { // 
-    Serial.print("Incoming message -->");
+    Serial.print("Incoming message --> ");
     msg[msglen] = '\0';
-Serial.println((char *)msg);
+    Serial.println((char *)msg);
+
+    // สถานะ LED บน NodeMCU ที่แสดงผล จะติดก็ต่อเมื่อสั่ง LOW 
+    // แต่ถ้าเป็น LED ที่ต่อแยก จะต้องสั่งเป็น HIGH 
     if(*(char *)msg == '1'){
-        digitalWrite(LED_BUILTIN, LOW);   // LED on
-        //microgear.chat(TargetWeb,"1");
-        //send_data("ESP_LED_ON");
-        send_json("ESP LED ON");
+        stateLED = 1;
+        digitalWrite(LEDPIN, HIGH); // LED on
+        //microgear.chat(neighbor,"1");
     }else{
-        digitalWrite(LED_BUILTIN, HIGH);  // LED off
-      //microgear.chat(TargetWeb,"0");
-      //send_data("ESP_LED_OFF");
-      send_json("ESP LED OFF");
+        stateLED = 0;
+        digitalWrite(LEDPIN, LOW); // LED off
+        //microgear.chat(neighbor,"0");
     }
 }
 
 void onConnected(char *attribute, uint8_t* msg, unsigned int msglen) {
     Serial.println("Connected to NETPIE...");
-    microgear.setName(ALIAS);
+    microgear.setAlias(ALIAS);
 }
 
 
 void setup() {
+    // กำหนดฟังก์ชั้นสำหรับรับ event callback 
     microgear.on(MESSAGE,onMsghandler);
     microgear.on(CONNECTED,onConnected);
 
+    // กำหนด baud rate สำหรับการสื่อสาร
     Serial.begin(115200);
     Serial.println("Starting...");
 
-    pinMode(LED_BUILTIN, OUTPUT);
-  
+    dht.begin(); // setup ตัวแปรสำหรับอ่านค่า DHT Sensor
+
+    // กำหนดชนิดของ PIN (ขาI/O) เช่น INPUT, OUTPUT เป็นต้น
+    pinMode(LEDPIN, OUTPUT);          // LED pin mode กำหนดค่า
+    stateLED = digitalRead(LEDPIN);
+    pinMode(SWITCHPIN, INPUT);        // Switch pin mode รับค่า
+
+    // เชื่อมต่อ wifi
     if (WiFi.begin(ssid, password)) {
         while (WiFi.status() != WL_CONNECTED) {
-            delay(500);
+            delay(1000);
             Serial.print(".");
         }
     }
 
-Serial.println("WiFi connected");
-    Serial.println("IP address: ");
-    Serial.println(WiFi.localIP());
-
-    microgear.init(KEY,SECRET,ALIAS);
-    microgear.connect(APPID);
-     digitalWrite(LED_BUILTIN, HIGH);   // LED on
+    
+    microgear.init(KEY,SECRET,ALIAS);   // กำหนดค่าตันแปรเริ่มต้นให้กับ microgear
+    microgear.connect(APPID);           // ฟังก์ชั่นสำหรับเชื่อมต่อ NETPIE
 }
 
-void send_json(String data){
-  StaticJsonBuffer<300> JSONbuffer;   //Declaring static JSON buffer
-    JsonObject& JSONencoder = JSONbuffer.createObject(); 
- 
-    JSONencoder["ESP"] = data;
- 
-    JsonArray& values = JSONencoder.createNestedArray("values"); //JSON array
-    values.add(20); //Add value to array
-    values.add(21); //Add value to array
-    values.add(23); //Add value to array
- 
- 
-    char JSONmessageBuffer[300];
-    JSONencoder.prettyPrintTo(JSONmessageBuffer, sizeof(JSONmessageBuffer));
-    Serial.println(JSONmessageBuffer);
- 
-    HTTPClient http;    //Declare object of class HTTPClient
- 
-    http.begin(host);      //Specify request destination
-    http.addHeader("Content-Type", "application/json");  //Specify content-type header
- 
-    int httpCode = http.POST(JSONmessageBuffer);   //Send the request
-    String payload = http.getString();                                        //Get the response payload
- 
-    Serial.println(httpCode);   //Print HTTP return code
-    Serial.println(payload);    //Print request response payload
- 
-    http.end();  //Close connection
-}
 void loop() {
-    if (microgear.connected()) {
-        Serial.println("..."); 
+    if (microgear.connected()) { // microgear.connected() เป็นฟังก์ชั่นสำหรับตรวจสอบสถานะการเชื่อมต่อ
+        // microgear.loop() เป็นฟังก์ชั่นสำหรับทวนสถานะการเชื่อมต่อกับ NETPIE (จำเป็นต้องมีใช้ loop)
         microgear.loop();
         timer = 0;
+        int state = digitalRead(SWITCHPIN);
+        if(state==LOW && !stateChange){
+          stateChange = true;
+        }
+
+        if(state==HIGH && stateChange) {
+          stateChange = false;
+          stateLED = !stateLED;
+          digitalWrite(LEDPIN, stateLED);
+//          microgear.chat(neighbor,stateLED);
+        }
+        
+        if(millis()-lastTimeDHT > 1000){
+          lastTimeDHT = millis();
+          
+          float h = dht.readHumidity();     // อ่านค่าความชื้น
+          float t = dht.readTemperature();  // อ่านค่าอุณหภูมิ
+          
+          Serial.print("Humidity: ");
+          Serial.print(h);
+          Serial.print(" %RH , ");
+          Serial.print("Temperature: ");
+          Serial.print(t);
+          Serial.println(" *C ");
+
+          // ตรวจสอบค่า humid และ temp เป็นตัวเลขหรือไม่
+          if (isnan(h) || isnan(t)) {
+            Serial.println("Failed to read from DHT sensor!");
+          }else{
+            humid = h;
+            temp = t;
+
+            String valuePublish = (String)humid+","+(String)temp+",";
+            Serial.print("Sending --> ");
+            Serial.println(valuePublish);
+            microgear.publish(topicPublish,valuePublish);
+          }
+        }
+
+        // FEED Code
+        // FEED Code END
     }
     else {
         Serial.println("connection lost, reconnect...");
@@ -108,6 +138,6 @@ void loop() {
             timer = 0;
         }
         else timer += 100;
+        delay(100);
     }
-    delay(100);
 }
